3eefabcf4a4d0618a7fd61f9a50abb349a2930ad
Nnenna Edeh <nnennaedeh54@gmail.com>
2020-02-26 13:51:56 +0100

    the assignment

---
+++ cli.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+Author: Nnenna
+Date: 09/02/2020
+ */ 
+$firstname = "Nnenna"; //Declare your name 
+$lastname = "Onyechimereze";
+$YOB = 2000;
+$person = ['tall', 'fair', 'Nigerian'];
+$person1['height'] = "short";
+$person1['complexion'] = "dark";
+$person1['nationality'] = "Nigerian";
+print  'My name is ' . $firstname . ' ' .$lastname;
+echo 'I was born in '.$YOB .',and i am'  .(2020-$YOB). 'years old';
+echo "I am $person[0] and $person[1] living in $person[2]"; "\n";
+echo "I am $person1[height] and $person1[complexion] living in $person1[nationality]";
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$school = "The mobile prof";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Introduction</title>
+</head>
+<body>
+<h1>Welcome to <?php echo $school; ?></h1>
+</body>
+</html>
